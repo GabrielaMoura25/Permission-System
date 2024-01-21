a1a55723c047c28c5def5ace8bd95df64c91cffe
Gabriela Moura <gabrieladaniel49@hotmail.com>
2024-01-21 18:33:23 -0300

    add variable process.env to conect backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: process.env.BASE_URL,
     headers: {
         'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2lnbWEtZnJvbnQiLCJvd25lciI6InNpZ21hLWZyb250In0.K6v8Zl8w_Tds2C7WRKszRX9y8654PpVjC8CqaShjTsE'
     }
