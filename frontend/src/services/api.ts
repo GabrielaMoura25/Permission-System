56f353fe139396543f989f4a5566b489731127bd
Gabriela Moura <gabrieladaniel49@hotmail.com>
2024-01-21 20:08:07 -0300

    change name baseurl to front

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.DATABASE_URL,
     headers: {
         'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2lnbWEtZnJvbnQiLCJvd25lciI6InNpZ21hLWZyb250In0.K6v8Zl8w_Tds2C7WRKszRX9y8654PpVjC8CqaShjTsE'
     }
