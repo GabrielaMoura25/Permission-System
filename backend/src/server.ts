afecc38daef86ccc9000871bbb5f85c21b421ee9
Gabriela Moura <gabrieladaniel49@hotmail.com>
2024-01-22 12:37:45 -0300

    updating .env variables and server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import routesPlugin from './routes'
 
+const port = process.env.PORT != null ? Number(process.env.PORT) : 3333
+
 const app = Fastify({ logger: true })
 
 app.setErrorHandler(async (error, _, reply) => {
@@ -13,8 +15,10 @@ const start = async (): Promise<void> => {
   await app.register(routesPlugin)
 
   try {
-    await app.listen({ port: 3333, host: '0.0.0.0' })
+    await app.listen({ port })
+    console.log(`Server listening on port ${port}`)
   } catch (err) {
+    app.log.error(err)
     process.exit(1)
   }
 }
