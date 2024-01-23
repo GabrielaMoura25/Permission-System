7a1219f22fa1393fc5569822ab492dabecae6dfb
Gabriela Moura <gabrieladaniel49@hotmail.com>
2024-01-23 00:14:15 -0300

    add url back deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,15 +2,11 @@ import axios from 'axios';
 
 export const api = () => {
 
-  const baseURL = process.env.REACT_APP_API_URL;
-  console.log('string teste', process.env);
-  console.log('string teste', baseURL);
-  
-    
-  return axios.create({
-    baseURL,
-    headers: {
-      'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2lnbWEtZnJvbnQiLCJvd25lciI6InNpZ21hLWZyb250In0.K6v8Zl8w_Tds2C7WRKszRX9y8654PpVjC8CqaShjTsE'
-    }
-  })
+    const baseURL = 'https://sigma-software-task-production.up.railway.app'
+    return axios.create({
+      baseURL,
+      headers: {
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2lnbWEtZnJvbnQiLCJvd25lciI6InNpZ21hLWZyb250In0.K6v8Zl8w_Tds2C7WRKszRX9y8654PpVjC8CqaShjTsE'
+      }
+    })
 } 
