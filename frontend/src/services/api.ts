138cfff3806793f13645939910914e689284ff46
Gabriela Moura <gabrieladaniel49@hotmail.com>
2024-01-22 22:17:09 -0300

    update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: 'https://sigma-software-task-production.up.railway.app',
-    headers: {
+export const api = () => {
+
+    const baseURL = process.env.DATABASE_URL
+    console.log(baseURL);
+    
+    return axios.create({
+      baseURL,
+      headers: {
         'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU2lnbWEtZnJvbnQiLCJvd25lciI6InNpZ21hLWZyb250In0.K6v8Zl8w_Tds2C7WRKszRX9y8654PpVjC8CqaShjTsE'
-    }
-})
+      }
+    })
+} 
